e51959e70f8f9c7b47896504027793b4f1dacdb5
Foo Bar <crowley.13@gmx.de>
2015-05-06 13:57:44 +0200

    whole project except 'Erweiterungen'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsair::Application.config.secret_token = '73cc2e28c85019f4aa6664727e0dfb6c29b35fd5a4ae9d50d187eab5f6daab7c41189c341bb216375c5e5046d3c351aef6531030fbbc0325df2268ec1cacd371'
